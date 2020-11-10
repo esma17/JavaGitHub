a9a48e5b4a96dce5323458ea290d82587231aa2a
esma17 <milkysf@gmail.com>
2020-11-10 13:51:24 -0600

    committing again to see if its tracking only the file I care about

--- src/File1.java
+++ src/File1.java
@@ -1,6 +1,8 @@
 public class File1 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+
 
     }
 }
---
+++ src/file2.java
@@ -0,0 +1,9 @@
+public class file2 {
+    public static void main(String[] args) {
+        int a=0;
+        int b=9;
+        int c=89;
+        int max=a;
+        System.out.println((a+b+c));
+    }
+}
